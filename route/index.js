bb2623d8e1f535a5bf4465817d6096c3b9cec2e0
Jeff Johnson <jeff@tmlinc.ca>
2014-06-11 11:21:16 -0400

    Reformatted code and changed needle locagtion.

--- route/index.js
+++ route/index.js
@@ -1,40 +1,41 @@
 'use strict';
+
 var path = require('path');
 var util = require('util');
 var ScriptBase = require('../script-base.js');
 var angularUtils = require('../util.js');
- 
+
 
 var Generator = module.exports = function Generator() {
-  ScriptBase.apply(this, arguments);
-  this.hookFor('angular-ui-router:controller');
-  this.hookFor('angular-ui-router:view');
+    ScriptBase.apply(this, arguments);
+    this.hookFor('angular-ui-router:controller');
+    this.hookFor('angular-ui-router:view');
 };
 
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.rewriteAppJs = function () {
-  if (this.env.options.coffee) {
-    angularUtils.rewriteFile({
-      file: path.join(this.env.options.appPath, 'scripts/app.coffee'),
-      needle: '.otherwise',
-      splicable: [
-        '.state \'/' + this.name + '\',',
-        '  templateUrl: \'views/' + this.name + '.html\',',
-        '  controller: \'' + this._.classify(this.name) + 'Ctrl\''
-      ]
-    });
-  }
-  else {
-    angularUtils.rewriteFile({
-      file: path.join(this.env.options.appPath, 'scripts/app.js'),
-      needle: '.otherwise',
-      splicable: [
-        '.state(\'/' + this.name + '\', {',
-        '  templateUrl: \'views/' + this.name + '.html\',',
-        '  controller: \'' + this._.classify(this.name) + 'Ctrl\'',
-        '})'
-      ]
-    });
-  }
+    if (this.env.options.coffee) {
+        angularUtils.rewriteFile({
+            file: path.join(this.env.options.appPath, 'scripts/app.coffee'),
+            needle: '$stateProvider',
+            splicable: [
+              '.state \'/' + this.name + '\',',
+              '  templateUrl: \'views/' + this.name + '.html\',',
+              '  controller: \'' + this._.classify(this.name) + 'Ctrl\''
+            ]
+        });
+    }
+    else {
+        angularUtils.rewriteFile({
+            file: path.join(this.env.options.appPath, 'scripts/app.js'),
+            needle: '$stateProvider',
+            splicable: [
+              '.state(\'/' + this.name + '\', {',
+              '  templateUrl: \'views/' + this.name + '.html\',',
+              '  controller: \'' + this._.classify(this.name) + 'Ctrl\'',
+              '})'
+            ]
+        });
+    }
 };
