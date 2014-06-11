559fa09b671c5f11aa6cfd787c9948e361c2c1ce
Jeff Johnson <jeff@tmlinc.ca>
2014-06-11 16:18:06 -0400

    Adding ability to create states.

---
+++ state/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var path = require('path');
+var util = require('util');
+var ScriptBase = require('../script-base.js');
+var angularUtils = require('../util.js');
+
+
+var Generator = module.exports = function Generator() {
+    ScriptBase.apply(this, arguments);
+    this.hookFor('angular-ui-router:controller');
+    this.hookFor('angular-ui-router:view');
+};
+
+util.inherits(Generator, ScriptBase);
+
+Generator.prototype.rewriteAppJs = function () {
+    if (this.env.options.coffee) {
+        angularUtils.rewriteFile({
+            file: path.join(this.env.options.appPath, 'scripts/app.coffee'),
+            needle: '.state',
+            splicable: [
+              '.state \'/' + this.name + '\',',
+              '  templateUrl: \'views/' + this.name + '.html\',',
+              '  controller: \'' + this._.classify(this.name) + 'Ctrl\''
+            ]
+        });
+    }
+    else {
+        angularUtils.rewriteFile({
+            file: path.join(this.env.options.appPath, 'scripts/app.js'),
+            needle: '.state',
+            splicable: [
+              '.state(\'/' + this.name + '\', {',
+              '  templateUrl: \'views/' + this.name + '.html\',',
+              '  controller: \'' + this._.classify(this.name) + 'Ctrl\'',
+              '})'
+            ]
+        });
+    }
+};
