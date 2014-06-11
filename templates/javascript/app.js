695138198203ec5d8540bc9e54f7c10b2f07d9c8
Jeff Johnson <jeff@tmlinc.ca>
2014-06-11 11:21:56 -0400

    Reformattted and added missing semi-colons.

--- templates/javascript/app.js
+++ templates/javascript/app.js
@@ -1,30 +1,29 @@
 'use strict';
 
 <% if(uirouterModules){ %>
-  angular.module('<%= _.camelize(appname) %>App', [<%= angularModules %>])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-      .state('index', {
-        url: '/',
-        templateUrl: 'views/main.html',
-        controller:'MainCtrl'
-      })
-  })
+angular.module('<%= _.camelize(appname) %>App', [<%= angularModules %>])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+        $urlRouterProvider.otherwise('/');
+        
+        $stateProvider
+            .state('index', {
+                url: '/',
+                templateUrl: 'views/main.html',
+                controller:'MainCtrl'
+            });
+    });
 <% }else{ %>
-  angular.module('<%= _.camelize(appname) %>App', [<%= angularModules %>])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('<%= _.camelize(appname) %>App', [<%= angularModules %>])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
 <% } %>
-
-
-
